f14e9e555dde769edc388be0d8e2fa521f1f1aab
Rizki Saepul Aziz <rzkysa0@gmail.com>
2024-12-01 22:04:46 +0700

    Initial Commit

---
+++ tugas_git.py
@@ -0,0 +1,49 @@
+data_panen = {
+    'lokasi1': {
+        'nama_lokasi': 'Kebun A',
+        'hasil_panen':{
+            'padi': 1200,
+            'jagung': 800,
+            'kedelai': 500
+        }
+    },
+    'lokasi2': {
+        'nama_lokasi': 'Kebun B',
+        'hasil_panen': {
+            'padi' : 1500,
+            'jagung': 900,
+            'kedelai': 450
+        }
+    },
+    'lokasi3': {
+        'nama_lokasi': 'Kebun C',
+        'hasil_panen': {
+            'padi': 1100,
+            'jagung': 750,
+            'kedelai': 600
+        }
+    },
+    'lokasi4': {
+        'nama_lokasi': 'Kebun D',
+        'hasil_panen': {
+            'padi': 1300,
+            'jagung': 850,
+            'kedelai' : 550
+        }
+    },
+    'lokasi5': {
+        'nama_lokasi': 'Kebun E',
+        'hasil_panen': {
+            'padi': 1400,
+            'jagung': 950,
+            'kedelai': 480
+        }
+    }
+}
+
+# Nomor 1
+print("Nomor 1")
+for i,j in data_panen.items():
+    print(f"Lokasi : {j["nama_lokasi"]}")
+    for hasil in j["hasil_panen"].items():
+        print(f"Hasil Panen : {hasil}")
